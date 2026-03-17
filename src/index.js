59068ec80f32edd33b81224031156e25a536cd11
Aarush Kumar <2005aarushkumar@gmail.com>
2026-03-17 19:17:24 +0530

    feat: add stats display

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import { getText } from "./loader.js";
 import { mapFreq } from "./frequency.js";
+import { buildHuffmanTree, buildCodeMap, encode, decode } from "./huffman.js";
 
 const path = "../samples/sample.txt";
 const text = getText(path);
-const freq = mapFreq(text);
 
-console.log(freq);
+const root = buildHuffmanTree(mapFreq(text));
+
+const codeMap = buildCodeMap(root);
+const encoded = encode(text, codeMap);
+const decoded = decode(encoded, root);
+
+// Stats
+const originalBits = text.length * 8;
+const compressedBits = encoded.length;
+const compressionRatio = (originalBits / compressedBits).toFixed(2);
+const saving = ((1 - compressedBits / originalBits) * 100).toFixed(2);
+
+console.log("-".repeat(50));
+console.log("CODE TABLE");
+console.log("-".repeat(50));
+
+for (const [char, code] of codeMap) {
+    console.log(`'${char === "\n" ? "\\n" : char}' -> ${code}`);
+}
+
+console.log();
+console.log("-".repeat(50));
+console.log("FILE STATISTICS");
+console.log("-".repeat(50));
+
+console.log(`Original: ${originalBits} bits`);
+console.log(`Compressed: ${compressedBits} bits`);
+console.log(`Compression Ratio: ${compressionRatio}`);
+console.log(`Savings: ${saving}%`);
+console.log(`Integrity check: ${decoded === text ? "PASSSED" : "FAILED"}`);
